50dc491c6d684ffd2699622693da890d64a9a2db
Ian Restrepo <iy.restrepo02@ciaf.edu.co>
2023-08-20 17:38:31 -0500

    Logic Of greeting System Simplified and fixed -- 17:38 D-20

--- src/js/greetinLogic.js
+++ src/js/greetinLogic.js
@@ -4,26 +4,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (loggedInUserName) {
         const greetingElement = document.getElementById('Header_Title');
-        const greeting = getGreeting();
-        greetingElement.textContent = `${greeting}, ${loggedInUserName}!`;
-        userNameToShowElement.textContent = `${loggedInUserName}!`;
+        const greeting = getGreeting(loggedInUserName);
+
+        greetingElement.innerHTML = `${greeting}, <span class="Blue">${loggedInUserName}</span>.`;
     }
 });
 
-function getGreeting() {
+function getGreeting(userName) {
     const now = new Date();
     const hour = now.getHours();
 
     let greeting = "";
 
     if (hour >= 6 && hour < 12) {
-        greeting = "Good morning!";
+        greeting = `Good morning`;
     } else if (hour >= 12 && hour < 18) {
-        greeting = "Good afternoon!";
+        greeting = `Good afternoon`;
     } else {
-        greeting = "Good night!";
+        greeting = `Good night`;
     }
 
-    return greeting;
+    return `${greeting}`;
 }
-

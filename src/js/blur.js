e06b8c1edbdc4e82aae1b198d8c04f927594f8dd
Xenolfair <iemabg2020@gmail.com>
2023-08-20 15:36:10 -0500

    Fix2

--- src/js/blur.js
+++ src/js/blur.js
@@ -1,12 +1,10 @@
-// Función para aplicar temporalmente el desenfoque al body
 function applyBlurTemporarily() {
     document.body.classList.add('blur-effect');
   
-    // Después de un tiempo determinado, quitar la clase de desenfoque
+
     setTimeout(() => {
       document.body.classList.remove('blur-effect');
-    }, 1000); // Desenfoque durante 2 segundos
+    }, 1000);
   }
   
-  // Llamar a la función cuando sea necesario
   applyBlurTemporarily();
